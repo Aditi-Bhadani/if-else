86c9cfe623eb9441298dbc9e273af36cbe48bc14
Aditi-Bhadani <100485354+Aditi-Bhadani@users.noreply.github.com>
2022-02-27 16:04:10 +0530

    Add files via upload

---
+++ divisible by 2&3.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a no. "))
+if (a%2)==0:
+    print (a, "is divisible by 2")
+else :
+    print (a, "not divisible by 2")
+
+if (a%3)==0:
+    print (a, "is divisible by 3")
+else :
+    print (a, "not divisible by 3")
---
+++ divisible by 5.py
@@ -0,0 +1,5 @@
+a=int(input("Enter a no. "))
+if (a%5)==0:
+    print ("Hello")
+else :
+    print ("Bye")
