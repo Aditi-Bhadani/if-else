33d7705351f7fa0714dd0e6fe4a3347969d98151
Aditi-Bhadani <100485354+Aditi-Bhadani@users.noreply.github.com>
2022-03-02 14:07:26 +0530

    Add files via upload

---
+++ voting.py
@@ -0,0 +1,7 @@
+a= int(input("Enter the age of the person "))
+if a>18:
+    print ("The person is eligible for voting")
+elif a==18:
+    print ("The person is eligible for voting")
+else :
+    print ("The person is not eligible to vote")
