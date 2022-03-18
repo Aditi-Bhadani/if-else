4dcc5bd68c98871622fd1bd76b19eb95069d01f8
Sarathskb <21sarathkumar2000@gmail.com>
2022-03-18 15:25:50 +0530

    div l no

---
+++ Div l no.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a no: "))
+if (a%2)==0:
+    print (a, "is divisible by 5")
+else :
+    print (a, "not divisible by 9")
+
+if (a%3)==0:
+    print (a, "is divisible by 3")
+else :
+    print (a, "not divisible by2")
